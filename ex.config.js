177ec82d3c08c3409fe85acfc4997897308cf4f1
Faishall <admin@faishall.id>
2025-08-17 14:10:29 +0700

    #

---
+++ ex.config.js
@@ -0,0 +1,110 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import moment from 'moment-timezone'
+
+/*============= WAKTU =============*/
+let wibh = moment.tz('Asia/Jakarta').format('HH')
+let wibm = moment.tz('Asia/Jakarta').format('mm')
+let wibs = moment.tz('Asia/Jakarta').format('ss')
+let wktuwib = `${wibh} H ${wibm} M ${wibs} S`
+let wktugeneral = `${wibh}:${wibm}:${wibs}`
+
+let d = new Date(new Date + 3600000)
+let locale = 'id'
+let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+})
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+/*============= MAIN INFO =============*/
+global.pairing = '6288226390953'
+global.owner = [['6288226390953', 'ZIFA', true]]        
+global.mods = []
+global.prems = []
+global.nomorbot = '6288226390953'
+global.nomorown = '6288226390953'
+
+/*============= WATERMARK =============*/
+global.readMore = readMore
+global.author = 'ZIFA'
+global.namebot = 'GO-BOT '
+global.wm = '© GO-BOT By ZIFA'
+global.watermark = wm
+global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
+global.bottime = `T I M E : ${wktuwib}`
+global.stickpack = `Sticker Dibuat dengan 💖`
+global.stickauth = `© GO-BOT By ZIFA`
+global.week = `${week} ${date}`
+global.wibb = `${wktuwib}`
+
+//*============= SOSMED =============*/
+global.sig = 'https://www.instagram.com/yaelah_sal'
+global.sgh = 'https://github.com/gospel99'
+global.sgc = ''
+global.sgw = 'https://faishall.id'
+global.sdc = '-'
+global.sfb = 'https://www.facebook.com/fangblack.id'
+global.snh = 'https://www.instagram.com/yaelah_sal'
+
+/*============= DONASI =============*/
+global.qris = ''
+global.psaweria = 'https://saweria.co/yaelahsal'
+
+/*============= TAMPILAN =============*/
+global.dmenut = 'ଓ═┅═━–〈' //top
+global.dmenub = '┊↬' //body
+global.dmenub2 = '┊' //body for info cmd on Default menu
+global.dmenuf = '┗––––––––––✦' //footer
+global.dashmenu = '┅═┅═❏ *DASHBOARD* ❏═┅═┅'
+global.cmenut = '❏––––––『' //top
+global.cmenuh = '』––––––' //header
+global.cmenub = '┊✦ ' //body
+global.cmenuf = '┗━═┅═━––––––๑\n' //footer
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     '
+global.pmenus = '✦'
+global.htki = '––––––『' // Hiasan Titile (KIRI)
+global.htka = '』––––––' // Hiasan Title  (KANAN)
+global.lopr = 'Ⓟ' //LOGO PREMIUM ON MENU.JS
+global.lolm = 'Ⓛ' //LOGO LIMIT/FREE ON MENU.JS
+global.htjava = '⫹⫺'    //hiasan Doang :v
+global.hsquere = ['⛶', '❏', '⫹⫺']
+
+/*============= RESPON =============*/
+global.wait = 'Please Wait...'
+global.eror = 'Error!'
+
+global.APIs = {
+    ryzumi: 'https://api.ryzumi.vip',
+
+}
+
+global.APIKeys = {
+    // 'https://website': 'apikey'
+}
+
+/*============= OTHER =============*/
+global.dpptx = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.ddocx = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.dxlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.dpdf = 'application/pdf'
+global.drtf = 'text/rtf'
+
+global.thumblvlup = [
+    'https://i.pinimg.com/originals/a0/34/8a/a0348ae908d8ac4ced76df289eb41e1a.jpg',
+    'https://i.pinimg.com/originals/be/3b/47/be3b477371cc249e49fd0bb3284de7d7.jpg',
+    'https://i.pinimg.com/originals/63/c3/37/63c337596b3391df0e72a9729ceca7b6.jpg',
+    'https://i.pinimg.com/originals/db/ed/5a/dbed5afac55d266602d0ca0c67622bb9.jpg'
+]
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+    unwatchFile(file)
+    console.log(chalk.redBright("Update 'config.js'"))
+    import(`${file}?update=${Date.now()}`)
+})
